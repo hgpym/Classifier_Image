7b63b788e2becb83d48b190f50ce54f9f59a040a
Max <thesuperiorflashnew52@gmail.com>
2024-12-12 16:20:14 +0500

    DeleteGarbageCommit
    AddReadME_DeleteGarbageCode

--- image_classifier/classification/tasks.py
+++ image_classifier/classification/tasks.py
@@ -4,16 +4,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "image_classifier.settings")
 import django
 django.setup()
 
-
-import dramatiq
+from dramatiq import actor
 from classification.models import UploadedImage
 from classification.some_ml_library import classify  # Подключите библиотеку для классификации изображений
-from classification.google_drive_utils import upload_to_drive
 import logging
 
 logger = logging.getLogger(__name__)
 
-@dramatiq.actor
+@actor
 def classify_image(image_id):
     image_instance = UploadedImage.objects.get(id=image_id)
     result, confidence = classify(image_instance.image.path)
@@ -21,6 +19,3 @@ def classify_image(image_id):
     image_instance.confidence = confidence
     image_instance.save()
     logger.info(f"Классификация завершена для изображения {image_instance.id}")
-
-    # Загрузка в Google Drive
-    upload_to_drive(image_instance.image.path, image_instance.image.name)
